435d3d2fc71ab572ff6052d34dc22e31ba5ec7a9
Alumnos <Alumnos@VH205-15>
2018-08-21 22:07:01 -0500

    ejercicio clase 2

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -32,25 +32,32 @@ public:
 
 	//////////////////////////////////////////////
 
-	//Metodos de Acceso
-	
 	//Set (Asignar)
 
 	void set_x(int x) ;
 	void set_y(int y) ;
 
+	
 	//Get (Obtener)
 
 	int get_x() ;
 	int get_y() ;
 
 
+	//Metodos de Acceso
+
+	int Sumar();
+	int Restar();
+	int Multiplicar();
+	double Dividir();
+
+
 };
 
 A::A()
 {
-	x = 0;
-	y = 0;
+	x = 4;
+	y = 5;
 }
 
 A::~A()
@@ -66,6 +73,36 @@ void A::set_x(int valor)
 void A::set_y(int valor)
 {
 	this->y = valor;
+}
+
+int A::Sumar()
+{
+	
+	int suma;
+	suma = x + y;
+	return suma;
+
+}
+
+int A::Restar()
+{
+	int resta;
+	resta = x + y;
+	return resta;
+}
+
+int A::Multiplicar()
+{
+	int multiplica;
+	multiplica = x*y;
+	return multiplica;
+}
+
+double A::Dividir()
+{
+	double divide;
+	divide = double(x) / double(y);
+	return divide;
 }
 
 int A::get_x()
@@ -92,12 +129,20 @@ int main()
 
 	cout<< "\nAtributo X : " << objA->get_x();
 
-	objA->set_x(666);
+	// ejemplo  objA->set_x(666);
 
-	cout << "\nAtributo X : " << objA->get_x();
+	cout << "\nAtributo y : " << objA->get_y();
 
+	cout << "\nAtributo X : " << objA->Sumar();
+	cout << "\nAtributo X : " << objA->Restar();
+	cout << "\nAtributo X : " << objA->Multiplicar();
+	cout << "\nAtributo X : " << objA->Dividir();
 
-    
+	cout << "\nCambiar los valor :";
+	cout << "\nCambiar el valor de x : ";
+	int VarX;
+	cin >> VarX;
+	cout << "\nAtributo x : "; objA->set_x(VarX);
 
 
 
